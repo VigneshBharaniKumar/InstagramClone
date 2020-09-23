5033ca4b2c2112c45c9878559dabdd96bc68f2d0
VigneshBharaniKumar <vignesh.b.qa@gmail.com>
2020-09-24 00:02:17 +0530

    Added Text Views in Profile Tab

--- app/src/main/java/com/vignesh/instagramclone/ProfileTab.java
+++ app/src/main/java/com/vignesh/instagramclone/ProfileTab.java
@@ -7,58 +7,62 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class ProfileTab extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextInputLayout edtName_ProfileTab, edtUserName_ProfileTab, edtWebsite_ProfileTab, edtBio_ProfileTab
+            , edtEmailID_ProfileTab, edtPhoneNumber_ProfileTab, edtGender_ProfileTab;
+    private Button btnUpdate_ProfileTab;
+    private AutoCompleteTextView genderDropdown_ProfileTab;
+
+  private String[] gender = {"Male", "Female", "Others", "Prefer Not to Say"};
 
     public ProfileTab() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileTab newInstance(String param1, String param2) {
+    public static ProfileTab newInstance() {
         ProfileTab fragment = new ProfileTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtName_ProfileTab = view.findViewById(R.id.edtName_ProfileTab);
+        edtUserName_ProfileTab = view.findViewById(R.id.edtUserName_ProfileTab);
+        edtWebsite_ProfileTab = view.findViewById(R.id.edtWebsite_ProfileTab);
+        edtBio_ProfileTab = view.findViewById(R.id.edtBio_ProfileTab);
+        edtEmailID_ProfileTab = view.findViewById(R.id.edtEmailID_ProfileTab);
+        edtPhoneNumber_ProfileTab = view.findViewById(R.id.edtPhoneNumber_ProfileTab);
+        edtGender_ProfileTab = view.findViewById(R.id.edtGender_ProfileTab);
+        genderDropdown_ProfileTab = view.findViewById(R.id.genderDropdown_ProfileTab);
+        btnUpdate_ProfileTab = view.findViewById(R.id.btnUpdate_ProfileTab);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.dropdown_menu_popup_item, gender);
+        genderDropdown_ProfileTab.setAdapter(adapter);
+
+        return view;
+
     }
 }
